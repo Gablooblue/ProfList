23298d4e578e6a852a122880e16bb2d609e91c70
gablooblue <gabrielhenry.lopez@gmail.com>
2017-02-16 23:45:30 +0800

    Created screens for creating professors

--- resources/views/create_professor.blade.php
+++ resources/views/create_professor.blade.php
@@ -3,14 +3,30 @@
 @section('content')
 
 <div class = "container" style= "padding-top:70px;">
-	<div class = "panel default-panel">
-		<div class = "panel-heading">
-			Submit a professor
+	<div class = "col-md-8 col-md-offset-2">
+		<div class = "panel panel-default">
+			<div class = "panel-heading">
+				Submit a professor
+			</div>
+			<div class = "panel-body">
+				<div class = "form-horizontal" role="form" method="post" action={{url('/professor/create')}}>
+					{{ csrf_field() }
+					<div class = "form-group{{ $errors->has('name') ? ' has-error' : ''}}" >
+						<label for ="name" class=" col-md-4 control-label">Name</label>
+						<div class="col-md-6">
+							 <input type= "text" name = "name" class = "form-control" value="{{ old('name') }}" required autofocus/> 
+						</div>
+					</div>
+					<div class = "form-group {{$errors}}"	
+					<p>School: <input type = "dropdown" name = "name" </p>
+					<p>Class/Classes: <input type= "text" name= "class" class ="class" placeholder="Ex. Math 17, CMSC 21"> </p>
+					<p>Picture <!-- TODO Input file upload --> </p>
+					<p><button type = "submit" class="btn btn-primary">Submit</button></p>
+			
+				</div>
+			</div>
 		</div>
-		<div class = "panel-body">
-			<p>Name: <input type= "text" name = "name" class = "name"/> </p>
 		</div>
 	</div>
-</div>
 
 @endsection
