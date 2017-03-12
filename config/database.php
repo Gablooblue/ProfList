f8106597d0478f78e9ab2be5c96a042913d7b8e1
gablooblue <gabrielhenry.lopez@gmail.com>
2017-03-12 21:39:21 +0800

    updated db configs

--- config/database.php
+++ config/database.php
@@ -69,7 +69,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => parse_url(getenv("DATABASE_URL"))["host"],
-            'port' => env('DB_PORT', '5432'),
+            'port' => '5432',
             'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
             'username' => parse_url(getenv("DATABASE_URL"))["user"],
             'password' => parse_url(getenv("DATABASE_URL"))["pass"],
