a235fffa29a92b23e8d8f3e6ed6afd0a46e14868
gablooblue <gabrielhenry.lopez@gmail.com>
2017-03-13 19:11:39 +0800

    Added user page

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -2,15 +2,17 @@
 
 @section('content')
 <div class = "container" style = "padding-top:70px;">
-	<div class = "panel default-panel">
+	<!--<div class = "panel default-panel">
 		<!-- TODO
 			Add profile picture
 			Add amount of reviews
 			Add amount of submissions
-		-->
+		
 		<div class = "panel-body">
-			<h2> {{ $user->username() }}</h2>
+			<h2> {{ $user->username }}</h2>
+			<h3>More coming soon!</h3>
 		</div>
-	</div>
+	</div>-->
+	<h3 class = "text-center">Coming soon!</h3>
 </div>
 @endsection
