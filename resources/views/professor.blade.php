9e3c471e3bfcc05bf3a29cac88bafed2652e3915
gablooblue <gabrielhenry.lopez@gmail.com>
2017-08-03 22:26:43 +0800

    Fixed view

--- resources/views/professor.blade.php
+++ resources/views/professor.blade.php
@@ -37,11 +37,6 @@
 	<div class = "text-center">
 		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#review">Write a review</button>
 	</div>
-	@else
-		<div class = "text-center">
-			<h4><a href = "{{ url('login') }}" style="color:#FFDD00;">Login</a> or <a href="{{url('register')}}" style = "color:#FFDD00">Sign up</a> to post a review</h4>
-		</div>
-	@endif
 	<div class = "modal fade" id = "review" role="dialog">
 		<div class = "modal-dialog">
 		<div class = "modal-content">
